4f4208f90279239ea4bac5affe6f8056dce29239
Naveed Jamali <naveedjamali@outlook.com>
2022-12-28 17:52:41 +0500

    imp

--- src/components/car.js
+++ src/components/car.js
@@ -4,26 +4,26 @@ class Car extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
       color: "red",
-     
     };
   }
 
-  shouldComponentUpdate(){
+  shouldComponentUpdate() {
     return false;
   }
 
-  changeColor = ()=> {
-    this.setState({ color: this.state.color=== "Green"?'Red':'Green' });
-  }
-  
+  changeColor = () => {
+    this.setState({ color: this.state.color === "red" ? "Green" : "red" });
+  };
 
   render() {
     return (
       <>
         <h1>I am a {this.state.color} color car from class component</h1>
-        <button onClick={this.changeColor}> Make it {this.state.color==='Red'?"Green":'Red'}</button>
+        <button onClick={this.changeColor}>
+          {" "}
+          Make it {this.state.color === "Red" ? "Green" : "Red"}
+        </button>
       </>
     );
   }
