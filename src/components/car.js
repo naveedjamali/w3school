68daab4ee4175ccc194710889f2e1aaa67f80839
Naveed Jamali <naveedjamali@outlook.com>
2022-12-28 13:39:43 +0500

    component lifecycle

--- src/components/car.js
+++ src/components/car.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 
 class Car extends React.Component {
-    constructor(props) {
-            super(props);
-            this.state = {
-                color: 'red',
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      color: "red",
+      model: "Mustang",
+      year: 1964,
+    };
+  }
 
-            }}
-
-            render(){
-                return (<>
-                <h1>I am a {this.state.color} color car from class component</h1></>)
-            }
-           
-
-           
+  changeColor = ()=> {
+    this.setState({ color: this.state.color=== "Green"?'Red':'Green' });
+  }
+  componentDidMount(){
+setTimeout(()=>this.setState({color:'Yellow'}),2000);
+  }
+  render() {
+    return (
+      <>
+        <h1>I am a {this.state.color} color car from class component</h1>
+        <button onClick={this.changeColor}> Make it {this.state.color==='Red'?"Green":'Red'}</button>
+      </>
+    );
+  }
 }
 
 export default Car;
