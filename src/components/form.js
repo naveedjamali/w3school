a55a544f287e840cbeac4d6fe1cb753d5d00414a
Naveed Jamali <naveedjamali@outlook.com>
2023-01-02 16:37:06 +0500

    react forms

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,28 @@
+import React from "react";
+import { useState } from "react";
+
 export default function MyForm(props) {
+  const [name, setName] = useState("");
+
   return (
     <>
-      <form action="#" method="GET">
-        <label>
-          <input type="text" name="myText" />
-        </label>
-        <button type="submit">OK</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          alert(name)
+        }}
+      >
+        <label for="myText">Enter you name </label>
+
+        <input
+          value={name}
+          type="text"
+          name="myText"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="submit" value="Submit" />
       </form>
+      <h1>{name}</h1>
     </>
   );
 }
