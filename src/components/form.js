8a57a1dcaf728ff5ace71c036f2b3485bc94dfb3
Naveed Jamali <naveedjamali@outlook.com>
2023-01-02 16:54:40 +0500

    react forms

--- src/components/form.js
+++ src/components/form.js
@@ -2,27 +2,42 @@ import React from "react";
 import { useState } from "react";
 
 export default function MyForm(props) {
-  const [name, setName] = useState("");
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  };
 
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          alert(name)
-        }}
-      >
-        <label for="myText">Enter you name </label>
-
-        <input
-          value={name}
-          type="text"
-          name="myText"
-          onChange={(e) => setName(e.target.value)}
-        />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your name:
+          <input
+            type="text"
+            name="username"
+            value={inputs.username || ""}
+            onChange={handleChange}
+          ></input>
+        </label>
+        <label>
+          Enter your age:{" "}
+          <input
+            type="number"
+            name="age"
+            value={inputs.age || ""}
+            onChange={handleChange}
+          ></input>
+        </label>
         <input type="submit" value="Submit" />
       </form>
-      <h1>{name}</h1>
     </>
   );
 }
