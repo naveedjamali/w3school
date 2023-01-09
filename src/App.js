2604897320f3cd5a1944cc39e5959ba5d5f312eb
Naveed Jamali <naveedjamali@outlook.com>
2023-01-09 17:17:31 +0500

    todos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import { useState } from "react";
 import { ReactDOM } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blog";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+import Todo from "./todo";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => {
+  const [count,setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = ()=>{
+    setCount((c)=>c+1);
+  };
+  return <>
+  <Todo todos={todos}/>
+  <hr/>
+  <div>
+    Count: {count}
+    <button onClick={increment}>+</button>
+  </div>
+  
+  </>;
+};
+
+
+export default App;
