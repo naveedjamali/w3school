9599ae964df7f3fa97a1b78ef1e84f04749d7d0b
Naveed Jamali <naveedjamali@outlook.com>
2023-01-15 18:00:34 +0500

    scss in react js

--- src/components/car.js
+++ src/components/car.js
@@ -1,7 +1,9 @@
 import st from './car-style.module.css'
+import './my-style.scss';
 
 export default function Car(){
     return (<>
+    <h1>A Big car Heading</h1>
     <p className={st.bigblue}>A Big Blue Car</p>
     </>);
 }
